20d51ea1f07a00be74b5c6006adbb74846aea1f6
AkashKank <akashkank4@gmail.com>
2026-02-10 13:58:35 +0530

    changes

--- hrms/views/employee_views.py
+++ hrms/views/employee_views.py
@@ -767,4 +767,3 @@ class LeaveRequestView(APIView):
 #                 return Response({"message": "HR approved"})
 #         except Exception as e:
 #             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
